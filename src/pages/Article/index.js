1ff4740aa08c99e0dfc39b4f8455c0a5a907f5ec
Sisiy <2424005179@qq.com>
2023-04-13 01:09:39 +0800

    富文本编辑器

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,9 +1,11 @@
-import { Card, Breadcrumb, Form, Button, Radio, Select, DatePicker, Table, Tag, Space } from 'antd'
+import { Popconfirm, Card, Breadcrumb, Form, Button, Radio, Select, DatePicker, Table, Tag, Space } from 'antd'
 import { Link } from 'react-router-dom'
 import 'moment/locale/zh-cn'
 import locale from 'antd/es/date-picker/locale/zh_CN'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
-
+import { useEffect, useState } from 'react'
+import { http } from '../../utils'
+import { history } from '../../utils/history'
 const { Option } = Select
 const { RangePicker } = DatePicker
 const Article = () => {
@@ -47,33 +49,102 @@ const Article = () => {
       render: data => {
         return (
           <Space size="middle">
-            <Button type="primary" shape='circle' icon={<EditOutlined />}></Button>
-            <Button
-              type="primary"
-              danger
-              shape="circle"
-              icon={<DeleteOutlined />}
-            />
+            <Button type="primary" shape='circle' icon={<EditOutlined />} onClick={() => history.push(`/home/publish?id=${data.id}`)}></Button>
+            <Popconfirm
+              title="确认删除该条文章吗?"
+              onConfirm={() => delArticle(data)}
+              okText="确认"
+              cancelText="取消"
+            >
+              <Button
+                type="primary"
+                danger
+                shape="circle"
+                icon={<DeleteOutlined />}
+              />
+            </Popconfirm>
           </Space>
 
         )
       }
     }
   ]
-  const data = [
-    {
-      id: '8218',
-      comment_count: 0,
-      cover: {
-        images: ['http://geek.itheima.net/resources/images/15.jpg'],
-      },
-      like_count: 0,
-      pubdate: '2019-03-11 09:00:00',
-      read_count: 2,
-      status: 2,
-      title: 'wkwebview离线化加载h5资源解决方案'
+
+
+  // 频道选项
+  const [channels, setChannels] = useState([])
+  useEffect(() => {
+    async function fetchChannels() {
+      const res = await http.get('/channels')
+      setChannels(res.data.data.channels)
     }
-  ]
+    fetchChannels()
+
+  }, [])
+
+  // 文章列表
+  const [article, setArticleList] = useState({
+    list: [],
+    count: 0
+  })
+
+  // 参数管理
+  const [params, setParams] = useState({
+    page: 1,
+    per_page: 10
+  })
+
+  // 发送请求接口
+  useEffect(() => {
+    async function fetchArticleList() {
+      const res = await http.get('/mp/articles', { params })
+      const { results, total_count } = res.data.data
+      setArticleList({
+        list: results,
+        count: total_count
+      })
+    }
+    fetchArticleList()
+  }, [params])
+
+  // 筛选功能
+  const onSearch = values => {
+    const { status, channel_id, date } = values
+    // 格式化
+    const _params = {}
+    // 赋值
+    _params.status = status
+    if (channel_id) _params.channel_id = channel_id
+    if (date) {
+      _params.begin_pubdate = date[0].format('YYYY-MM-DD')
+      _params.end_pubdate = date[1].format('YYYY-MM-DD')
+    }
+    // 修改参数
+    setParams({
+      ...params,
+      ..._params
+    })
+  }
+
+  // 分页
+  const pageChange = (page) => {
+    setParams({
+      ...params,
+      page
+    })
+  }
+  // 删除
+  const delArticle = async (data) => {
+    await http.delete(`/mp/articles/${data.id}`)
+    // 更新列表
+    setParams({
+      page: 1,
+      per_page: 10
+    })
+  }
+
+
+
   return (
     <div>
       <Card
@@ -87,7 +158,7 @@ const Article = () => {
         }
         style={{ marginBottom: 20 }}>
 
-        <Form initialValues={{ status: null }}>
+        <Form initialValues={{ status: null }} onFinish={onSearch}>
           <Form.Item label="状态" name="status">
             <Radio.Group>
               <Radio value={null}>全部</Radio>
@@ -102,11 +173,16 @@ const Article = () => {
           <Form.Item label="频道" name="channel_id">
             <Select
               placeholder="请选择文章频道"
-              defaultValue="lucy"
               style={{ width: 120 }}
             >
-              <Option value="jack">Jack</Option>
-              <Option value="lucy">Lucy</Option>
+              {channels.map(item => (
+                <Option key={item.id} value={item.id}>
+                  {item.name}
+                </Option>
+              ))}
+
+              {/* <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option> */}
             </Select>
           </Form.Item>
 
@@ -123,8 +199,15 @@ const Article = () => {
         </Form>
       </Card>
 
-      <Card title={`根据筛选条件共查询到 count 条结果：`}>
-        <Table rowKey="id" columns={column} dataSource={data} />
+      <Card title={`根据筛选条件共查询到${article.count}条结果：`}>
+        <Table rowKey="id" columns={column} dataSource={article.list}
+          pagination={{
+            position: ['bottomCenter'],
+            current: params.page,
+            pageSize: params.per_page,
+            onChange: pageChange
+          }}
+        />
       </Card>
     </div>
   )
