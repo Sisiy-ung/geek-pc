b2d23570d96fa8b307618364e9b34473d71adfbe
Sisiy <2424005179@qq.com>
2023-04-14 01:08:14 +0800

    表单提交

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -12,10 +12,50 @@ import {
 import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { useEffect, useState } from 'react'
+import { http } from '../../utils'
 
 const { Option } = Select
 
 const Publish = () => {
+
+  // 频道
+  const [channels, setChannels] = useState([])
+  useEffect(() => {
+    async function fetchChannels() {
+      const res = await http.get('/channels')
+      setChannels(res.data.data.channels)
+    }
+    fetchChannels()
+  }, [])
+
+  // fileList
+  const [fileList, setFileList] = useState([])
+  const onUploadChange = info => {
+    console.log(info, 'info')
+    const fileList = info.fileList.map(file => {
+      if (file.response) {
+        return {
+          url: file.response.data.url
+        }
+      }
+      return file
+    })
+    setFileList(fileList)
+  }
+
+  // 图片数量
+  const [imgCount, setImgCount] = useState(1)
+  const changeType = e => {
+    const count = e.target.value
+    setImgCount(count)
+  }
+
+  // 提交
+
+
   return (
     <div className="publish">
       <Card
@@ -46,34 +86,56 @@ const Publish = () => {
             rules={[{ required: true, message: '请选择文章频道' }]}
           >
             <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-              <Option value={0}>推荐</Option>
+              {
+                channels.map(item => (
+                  <Option key={item.id} value={item.id}>
+                    {item.name}</Option>
+                ))
+              }
             </Select>
           </Form.Item>
 
           <Form.Item label="封面">
             <Form.Item name="type">
-              <Radio.Group>
+              <Radio.Group onChange={changeType}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
                 <Radio value={0}>无图</Radio>
               </Radio.Group>
             </Form.Item>
-            <Upload
-              name="image"
-              listType="picture-card"
-              className="avatar-uploader"
-              showUploadList
-            >
-              <div style={{ marginTop: 8 }}>
-                <PlusOutlined />
-              </div>
-            </Upload>
+            {
+              imgCount > 0 && (
+                <Upload
+                  name="image"
+                  listType="picture-card"
+                  className="avatar-uploader"
+                  showUploadList
+                  action="http://geek.itheima.net/v1_0/upload"
+                  fileList={fileList}
+                  onChange={onUploadChange}
+                  multiple={imgCount > 1}
+                  maxCount={imgCount}
+
+                >
+                  <div style={{ marginTop: 8 }}>
+                    <PlusOutlined />
+                  </div>
+                </Upload>
+              )
+            }
+
           </Form.Item>
           <Form.Item
             label="内容"
             name="content"
             rules={[{ required: true, message: '请输入文章内容' }]}
-          ></Form.Item>
+          >
+            <ReactQuill
+              className='publish-quill'
+              themw="snow"
+              placeholder='请输入文章内容'
+            />
+          </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
             <Space>
